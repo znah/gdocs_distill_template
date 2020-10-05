31b1a9b35f6bfbfe09a5013096c9756e3ab10127
Eyvind Niklasson <eyvind@google.com>
2020-10-05 16:15:05 +0200

    update bin/dev.py
    - don't overwrite article.html on disk, load new changes in memory instead
    - don't use os.system to get github path, read .git/config instead

--- bin/dev.py
+++ bin/dev.py
@@ -1,14 +1,21 @@
-'''Advanced web-development server'''
+''' "Advanced web-development server" '''
 
 from __future__ import print_function
 
 import os, glob
 import six
+from six.moves.urllib.request import urlopen, Request
 if six.PY3:
     from http.server import SimpleHTTPRequestHandler, test
 else:
     from SimpleHTTPServer import SimpleHTTPRequestHandler, test
 import re
+
+# https://medium.com/@moreless/how-to-fix-python-ssl-certificate-verify-failed-97772d9dd14c 
+import ssl
+if (not os.environ.get('PYTHONHTTPSVERIFY', '') and
+getattr(ssl, '_create_unverified_context', None)):
+    ssl._create_default_https_context = ssl._create_unverified_context
 
 def copy_byte_range(infile, outfile, start=None, stop=None, bufsize=16*1024):
     '''Like shutil.copyfileobj, but only copy a range of the streams.
@@ -40,26 +47,40 @@ def parse_byte_range(byte_range):
         raise ValueError('Invalid byte range %s' % byte_range)
     return first, last
 
-def write_file(fname, fout):
-    for s in open(fname):
+def write_file(fname, fout, article_html):
+    for s in open(fname) if (fname != "../article.html" or article_html is None) else article_html:
+        if not (isinstance(s, str)):
+            s = s.decode('utf-8')
         if s.strip().startswith('{% include '):
             fn = '../'+s.split()[2]
-            write_file(fn, fout)
+            write_file(fn, fout, article_html)
         else:
             fout.write(s)
 
 def build():
-    if os.environ.get("GIT_API_KEY_SELFORG") is not None:
-        print("using api key in environment")
-        os.system('''curl -o ../article.html \
-        --header 'Authorization: token ''' + os.environ.get("GIT_API_KEY_SELFORG") + '''' \
-        --header 'Accept: application/vnd.github.v3.raw' \
-        --location https://api.github.com/repos/$(git remote -v | head -n 1 | sed 's/.*github\.com:\(.*\)\.git.*/\\1/')/contents/article.html \
-        ''')
-    else:
-        print("no api key available")
+    article_html = None
+    try:
+        found_remote = False
+        remote_url = None
+        for s in open('../.git/config'):
+            if s.strip() == '[remote "origin"]':
+                found_remote = True
+            if found_remote and s.strip().startswith('url'):
+                remote_url = s.strip().split('=', 1)[1]
+        uname_and_repo = re.match(r".*github\.com[:|/](.*)\.git.*", remote_url).group(1)
+        req = Request('https://api.github.com/repos/' + uname_and_repo + '/contents/article.html')
+        req.add_header('Authorization', 'token ' + os.environ.get("GIT_API_KEY_SELFORG"))
+        req.add_header('Accept', 'application/vnd.github.v3.raw')
+        article_html = urlopen(req)
+        if article_html.getcode() != 200:
+          print('github response was not 200')
+          article_html = None
+    except Exception as e:
+        article_html = None
+        print("failed to fetch latest article.html. error:");
+        print(e)
     with open('index.html', 'w') as fout:
-      write_file('../main.html', fout)
+      write_file('../main.html', fout, article_html)
     print('build finished')
 
 
